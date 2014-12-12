e58b362dfba7bac7eff0f3b0203e59734546e2ca
Sebastien Carreau <sebastien.carreau@tocea.com>
2014-12-12 18:46:00 +0100

    update opencsv version from 2.3 to 3.1

--- src/main/java/org/josql/csv/JoCsvToBean.java
+++ src/main/java/org/josql/csv/JoCsvToBean.java
@@ -1,30 +1,29 @@
 package org.josql.csv;
 
+import com.opencsv.bean.CsvToBean;
 import java.beans.PropertyDescriptor;
 import java.util.Map;
 
-import au.com.bytecode.opencsv.bean.CsvToBean;
-
 public class JoCsvToBean extends CsvToBean<Object> {
 
-	private Map<Class<?>, StringConverter<?>> converters;
-	
-	public JoCsvToBean(final Map<Class<?>, StringConverter<?>> _converters) {
-		
-		converters = _converters;
-		
-	}
-	
-	@Override
-    protected Object convertValue(final String value, final PropertyDescriptor prop) throws InstantiationException,IllegalAccessException {
-		
-		StringConverter<?> converter = converters.get(prop.getPropertyType());
-		
-		if (converter != null) {			
-			return converter.convertValue(value);				
-		}
-		
+    private Map<Class<?>, StringConverter<?>> converters;
+
+    public JoCsvToBean(final Map<Class<?>, StringConverter<?>> _converters) {
+
+        converters = _converters;
+
+    }
+
+    @Override
+    protected Object convertValue(final String value, final PropertyDescriptor prop) throws InstantiationException, IllegalAccessException {
+
+        StringConverter<?> converter = converters.get(prop.getPropertyType());
+
+        if (converter != null) {
+            return converter.convertValue(value);
+        }
+
         return super.convertValue(value, prop);
     }
-	
+
 }
